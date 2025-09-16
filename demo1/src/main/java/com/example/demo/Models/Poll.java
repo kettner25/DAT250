b3fd8bb6f5acd4f3717c8f71db91c9cab799f4a7
Lubos Kettner <lubos.kettner@gmail.com>
2025-09-16 14:09:39 +0200

    hw03: Adding Database test ... finally running

--- demo1/src/main/java/com/example/demo/Models/Poll.java
+++ demo1/src/main/java/com/example/demo/Models/Poll.java
@@ -7,6 +7,7 @@ import lombok.ToString;
 
 import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 @Data
@@ -33,5 +34,30 @@ public class Poll {
         if (voteOpts.size() < 2) return false;
 
         return true;
+    }
+
+    /**
+     *
+     * Adds a new option to this Poll and returns the respective
+     * VoteOption object with the given caption.
+     * The value of the presentationOrder field gets determined
+     * by the size of the currently existing VoteOptions for this Poll.
+     * I.e. the first added VoteOption has presentationOrder=0, the secondly
+     * registered VoteOption has presentationOrder=1 ans so on.
+     */
+    public VoteOption addVoteOption(String caption) {
+        var voteOption = new VoteOption();
+
+        voteOption.setCaption(caption);
+
+        int order = 0;
+        var a = voteOpts.stream().max(Comparator.comparing(s -> s.getPresentationOrder())).orElse(null);
+        if (a != null) order = a.getPresentationOrder() + 1;
+
+        voteOption.setPresentationOrder(order);
+
+        voteOpts.add(voteOption);
+
+        return voteOption;
     }
 }
--- demo1/src/main/java/com/example/demo/Models/User.java
+++ demo1/src/main/java/com/example/demo/Models/User.java
@@ -2,11 +2,14 @@ package com.example.demo.Models;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
+@NoArgsConstructor
 public class User {
     //Unique
     private String username;
@@ -19,5 +22,49 @@ public class User {
         if (username == null || username.isEmpty() || email == null || email.isEmpty()) return false;
 
         return true;
+    }
+
+    /**
+     * Creates a new User object with given username and email.
+     * The id of a new user object gets determined by the database.
+     */
+    public User(String username, String email) {
+        this.username = username;
+        this.email = email;
+        this.created = new ArrayList<>();
+    }
+
+    /**
+     * Creates a new Poll object for this user
+     * with the given poll question
+     * and returns it.
+     */
+    public Poll createPoll(String question) {
+        var poll =  new Poll();
+        poll.setQuestion(question);
+
+        poll.setCreator(this);
+        poll.setPublishedAt(Instant.now());
+
+        created.add(poll);
+
+        return poll;
+    }
+
+    /**
+     * Creates a new Vote for a given VoteOption in a Poll
+     * and returns the Vote as an object.
+     */
+    public Vote voteFor(VoteOption option) {
+        var vote = new Vote();
+
+        vote.setOption(option);
+        vote.setUser(this);
+
+        vote.setPublishedAt(Instant.now());
+
+        voted.add(vote);
+
+        return vote;
     }
 }
